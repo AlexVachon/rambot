cbdd4cd360f8c1bc915fe12160d9fa11d1e19e30
avachon <alexandre.vachon@brizodata.com>
2025-03-01 10:51:15 -0500

    -tests

--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-# tests/test_utils.py
-import pytest
-from botasaurus.utils import read_json_file
-
-def test_read_json_file():
-    with pytest.mock.open() as m_open:
-        m_open.return_value.__enter__.return_value.read.return_value = '{"test": "data"}'
-        data = read_json_file("test.json")
-        assert data == {"test": "data"}
