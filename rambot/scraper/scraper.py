3f8032f910e25ed6d1e166bdd22ca4d5113fb5b3
AlexVachon <alex.vachon@outlook.com>
2025-08-09 16:21:44 -0400

    move _find_by_xpath logic in IScraper instead of Scraper

--- rambot/scraper/scraper.py
+++ rambot/scraper/scraper.py
@@ -3,11 +3,9 @@ import time
 import random
 import argparse
 
-from typing import Optional, List
+from typing import Optional
 
-from botasaurus_driver.driver import Driver, Wait, Element, make_element
-from botasaurus_driver import cdp
-from botasaurus_driver.core import util, element
+from botasaurus_driver.driver import Driver, Wait
 
 from .. import helpers
 from ..logging_config import update_logger_config, get_logger
@@ -222,51 +220,6 @@ class Scraper(IScraper):
                 raise Exception("No element found")
             return elements[0]
         return elements
-
-    def _find_by_xpath(self, query, root = None, timeout = 10) -> List[Element]:
-        results = []
-        start_time = time.time()
-        poll_interval = 0.1
-
-        while True:
-            results.clear()
-
-            if root is not None:
-                doc = root._elem._node
-            else:
-                doc = self.driver._tab.send(cdp.dom.get_document(depth=-1, pierce=True))
-
-            search_id, result_count = self.driver._tab.send(
-                cdp.dom.perform_search(query=query)
-            )
-
-            if result_count > 0:
-                node_ids = self.driver._tab.send(
-                    cdp.dom.get_search_results(
-                        search_id=search_id,
-                        from_index=0,
-                        to_index=result_count
-                    )
-                )
-
-                for node_id in node_ids:
-                    node = util.filter_recurse(doc, lambda n: n.node_id == node_id)
-                    if not node:
-                        continue
-                    internal_element = element.create(node, self.driver._tab, doc)
-                    elem = make_element(self, self.driver._tab, internal_element)
-                    results.append(elem)
-
-                if results:
-                    return results
-
-            elapsed = time.time() - start_time
-            if elapsed >= timeout:
-                break
-
-            time.sleep(poll_interval)
-
-        return results
 
     def click(self, query, by = By.XPATH, timeout = Wait.SHORT) -> bool:
         try:
